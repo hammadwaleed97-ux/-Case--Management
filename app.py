008f888c763260e84b61d03cb37ffdcce14e9cfc
hammadwaleed97-ux <hammadwaleed97@gmail.com>
2026-07-13 12:09:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -151,6 +151,7 @@ if st.session_state.page == "الرئيسية":
 # ================================================
 
 elif st.session_state.page == "تسجيل":
+    data = load_data()
     st.markdown("<div style='height:20px'></div>", unsafe_allow_html=True)
     st.markdown("<h2 style='color:#D4AF37; text-align:center'>➕ تسجيل القضايا</h2>", unsafe_allow_html=True)
     
