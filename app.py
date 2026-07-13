0444e4b424ab6d6308512ce11ef834ce0cbe2327
hammadwaleed97-ux <hammadwaleed97@gmail.com>
2026-07-13 20:36:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -179,10 +179,9 @@ if st.session_state.page == "الرئيسية":
         st.markdown('<div class="btn-report">', unsafe_allow_html=True)
         if st.button("📊 التقارير", use_container_width=True): st.session_state.page = "تقارير"; st.rerun()
         st.markdown('</div>', unsafe_allow_html=True)
-    with col2:
-        st.markdown('<div class="btn-lib">', unsafe_allow_html=True)
-        if st.button("📚 المكتبة القانونية", use_container_width=True): st.session_state.page = "مكتبة"; st.rerun()
-        st.markdown('</div>', unsafe_allow_html=True)
+    if st.button("📚 المكتبة القانونية", use_container_width=True): 
+    st.session_state.page = "المكتبة"  # زودت "ال"
+    st.rerun()
         
     col1, col2, col3 = st.columns([1,2,1])
     with col2:
