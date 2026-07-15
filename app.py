9f71176cfd1c8cd7513e1aa398ca0d23c39c24ce
hammadwaleed97-ux <hammadwaleed97@gmail.com>
2026-07-15 07:57:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -277,14 +277,15 @@ elif st.session_state.page == "تسجيل":
                 st.success(f"✅ تم حفظ القضية رقم {رقم} لسنة {سنة}")
                 st.session_state.page = "الحصر"; st.rerun()
 # ================================================
+    # ================================================
 # ========== الجزء الثالث: الحصر العام ============
 # ================================================
 elif st.session_state.page == "الحصر":
-    data = load_data()
+    data = st.session_state.get('data', load_data()) # <-- متغير
+    st.session_state.data = data # <-- سطر جديد
     st.markdown("<div style='height:20px'></div>", unsafe_allow_html=True)
     st.markdown("<h2 style='color:#FFFFFF; text-align:center'>📊 الحصر العام الخارجي</h2>", unsafe_allow_html=True)
     if st.button("⬅️ العودة للرئيسية", use_container_width=True): st.session_state.page = "الرئيسية"; st.rerun()
-
     # ======= السطرين الجداد دول بس =======
     if st.session_state.get('open_from_search', False):
         st.session_state.open_from_search = False
