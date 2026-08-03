f0bc6724b28d48d45ffac5e404b889e72d195ec0
hammadwaleed97-ux <hammadwaleed97@gmail.com>
2026-08-03 18:00:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1592,7 +1592,7 @@ elif st.session_state.page == "بحث":
                     # ======
 elif st.session_state.page == "التنبيهات":
     st.markdown("<h1 style='text-align:center; color:#C9A961;'>مركز التنبيهات</h1>", unsafe_allow_html=True)
-    if st.button("⬅️ العودة للرئيسية", use_container_width=True):
+         if st.button("⬅️ العودة للرئيسية", use_container_width=True):
     st.session_state.page = "الرئيسية"
     st.rerun()
     
