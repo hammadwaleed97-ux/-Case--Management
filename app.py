fed077d1c5f35052ed8b18ffd9d1fc2b6069e156
hammadwaleed97-ux <hammadwaleed97@gmail.com>
2026-08-06 00:11:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -844,12 +844,11 @@ if st.session_state.page == "الرئيسية":
         unsafe_allow_html=True
     )
         
-     for b in st.session_state.banners:
+    for b in st.session_state.banners:
         if b["expire"] > datetime.now():
             st.markdown(f'<div style="background:{b["color"]};padding:12px;border-radius:10px;margin:10px 0;text-align:center;font-weight:bold;color:#000;">📢 {b["text"]}</div>', unsafe_allow_html=True)
 
     col1, col2 = st.columns(2)
-
     with col1:
 
         st.markdown(
